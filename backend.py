c9a412bbe2fb28c88a32a7c0b34f0b43aa303341
ardit <arvisluo@gmail.com>
2023-01-19 01:55:17 -0500

    Initial commit

---
+++ backend.py
@@ -0,0 +1,19 @@
+import requests
+
+API_KEY="8a1c25a99b7e5f85446ca6177c1bf3b9"
+
+def get_data(place, forecast_days, kind):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_KEY}"
+    response = requests.get(url)
+    content = response.json()
+    filtered_data = data["list"]
+    nr_values = 8 * forecast_days
+    filtered_data = filtered_data[:nr_values]
+    if kind == "Temperature":
+        filtered_data = [dict["main"]["temp"] for dict in filtered_data]
+    if kind == "Sky":
+        filtered_data = [dict["weather"][0]["main"] for dict in filtered_data]
+    return filtered_data
+
+if __name__ == "__main__":
+    get_date(place)
